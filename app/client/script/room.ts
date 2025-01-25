eeb418a165ac7def0b020ca065d42a3effde1b26
mogataro <negishikouhei1013@gmail.com>
2025-01-25 10:29:18 +0900

    change: protocol修正

--- app/client/script/room.ts
+++ app/client/script/room.ts
@@ -2,8 +2,7 @@ import dayjs from 'dayjs';
 import type { MessageType, MessageJson } from '../../types/index.ts';
 
 const uri = new URL(window.location.href);
-const wsProtocol = uri.protocol === 'https' ? 'wss' : 'ws';
-
+const wsProtocol = uri.protocol === 'https:' ? 'wss' : 'ws';
 const ws = new WebSocket(`${wsProtocol}://${uri.hostname}`);
 
 let uuid = '';
